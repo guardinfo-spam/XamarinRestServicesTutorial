1e9d0023c1403df971bc77147d30e65aad02e074
eidand <eidand@yahoo.com>
2015-07-25 17:36:58 +0100

    WIndows Phone app

---
+++ XamarinRestServices.WindowsApp/XamarinRestServices.WindowsApp.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamarinRestServices.Models;
+using XamarinRestServices.RestServicePclLibrary;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace XamarinRestServices.WindowsApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            CallMyService();
+        }
+
+        public async void CallMyService()
+        {
+            ServiceWrapper serviceWrapper = new ServiceWrapper();
+            var model = new UserModel { Username = "demo", Password = "demo" };
+
+            var result = await serviceWrapper.GetData("test");
+            txtMessages.Text = "get call says: " + result;
+
+            result = await serviceWrapper.RegisterUserJsonRequest(model);
+            txtMessages.Text = "json post call says: " + result;
+
+            result = await serviceWrapper.RegisterUserFormRequest(model);
+            txtMessages.Text = "form post request says: " + result;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
